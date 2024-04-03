37daba46d71556929d0402343695b2280a656da5
An1by <mrkillermafia@gmail.com>
2024-04-03 15:31:07 +0500

    header change

--- web/api/server/HttpServer.js
+++ web/api/server/HttpServer.js
@@ -41,10 +41,10 @@ app.use(cors({
 }));
 
 app.all("*", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
     next();
 });
 
